f4da78ca158be4829ce80b14cd7d344fe7705d04
Phenny <mwaisakaphenny@gmail.com>
2025-02-11 05:10:33 +0300

    fetch image

--- src/main/java/com/mwaisaka/Ecommerce_Backend/controller/ProductController.java
+++ src/main/java/com/mwaisaka/Ecommerce_Backend/controller/ProductController.java
@@ -4,6 +4,7 @@ import com.mwaisaka.Ecommerce_Backend.model.Product;
 import com.mwaisaka.Ecommerce_Backend.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -47,5 +48,16 @@ public class ProductController {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
 
+    }
+
+    @GetMapping("/product/{productId}/image")
+    public ResponseEntity<byte[]> getImageByProductId(@PathVariable int productId){
+
+        Product product = service.getProductById(productId);
+        byte[] imageFile = product.getImageDate();
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.valueOf(product.getImageType()))
+                .body(imageFile);
     }
 }
